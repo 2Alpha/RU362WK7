232818bc0a49e0e4f3b5f4f45d1e8e70f7c57611
2Alpha <andrae3000@gmail.com>
2015-10-11 08:13:20 -0400

    1%

--- RU362WK7/Source.cpp
+++ RU362WK7/Source.cpp
@@ -32,3 +32,139 @@ struct residence
 	bool rented;
 };
 
+
+const int IGNORE_AMOUNT = 100;
+
+string showAddDeleteExit();
+string convert2UpperCase(string stringInput);
+bool ask2LoadExistingData();
+
+int main()
+{
+
+}
+
+//*************************************************************************
+//  FUNCTION:	  showAddDeleteExit
+//  DESCRIPTION:  Presents the user with the options of show, add, delete, or exit the program
+//  INPUT:        Parameters:  User input from console
+//
+//  OUTPUT: 	  Return value: string response2Question - response to question
+//*************************************************************************
+string showAddDeleteExit()
+{
+	string response2Question;			// Response to question
+	bool repeatQuestion;				// Repeat question
+	do {
+
+		cout << endl;
+		cout << "Please choose an option by entering its corresponding letter" << endl;
+		cout << "S = Show rentals to the Screen " << endl;
+		cout << "A = Add rentals to the array " << endl;
+		cout << "D = Delete a rental" << endl;
+		cout << "X = Exit the program" << endl;
+		cout << endl;
+		cout << "Please enter response here: ";
+		cin >> response2Question;
+		cin.ignore(IGNORE_AMOUNT, '\n');
+
+		response2Question = convert2UpperCase(response2Question);
+
+
+		if ((response2Question != "S") && (response2Question != "A") && (response2Question != "D") && (response2Question != "X"))
+		{
+			cout << endl;
+			cout << "ERROR! Unrecognized input, please try again." << endl;
+			cout << endl;
+			repeatQuestion = true;
+		}
+
+		else
+		{
+			repeatQuestion = false;
+		}
+	}
+
+	// while repeat question equals true
+	while (repeatQuestion == true);
+
+
+	return response2Question;
+
+}
+
+string convert2UpperCase(string stringInput)
+{
+	int i = 0;  // variabel used for tracking index location in array
+	char c;		// variable used to temporaliy store character from string
+
+	string upperCasedString;		// A string where all the letters are uppercase  
+
+	while (stringInput[i] != '\0')
+	{
+		// if character is a letter 
+		if (isalpha(stringInput[i]))
+		{
+			c = stringInput[i];
+			stringInput[i] = toupper(c);
+			i++;
+		}
+
+		else
+		{
+			i++;
+		}
+
+	}
+
+	upperCasedString = stringInput;
+
+	return upperCasedString;
+
+}
+
+bool ask2LoadExistingData()
+{
+	string promt1Response;
+	string fileName2Load; 
+	int numOfErrors;
+	bool ReadInputFile; 
+
+	do
+	{
+		numOfErrors = 0;
+		cout << endl;
+		cout << "Would you like to load existing data from a file (Y / N) ?  ";
+		cin >> promt1Response;
+
+		promt1Response = convert2UpperCase(promt1Response);
+
+
+		if ((promt1Response != "Y") && (promt1Response != "N"))
+		{
+			cout << endl;
+			cout << "ERROR! Unrecognized input, please try again." << endl;
+			numOfErrors++;
+		}
+
+		else if (promt1Response == "Y")
+		{
+			ReadInputFile = true;
+			cin >> fileName2Load; 
+		}
+
+
+		else if (promt1Response == "N")
+		{
+			cout << "Okay, lets start fresh!" << endl;  
+		}
+
+	}
+
+	// loop if error detected flag equals true 
+	while (numOfErrors > 0);
+
+	return ReadInputFile;
+
+
+}
