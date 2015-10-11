833d6ce60a729f45c6bd64b2b0176dd255ee2f03
2Alpha <andrae3000@gmail.com>
2015-10-11 07:40:08 -0400

    0%

---
+++ RU362WK7/Source.cpp
@@ -0,0 +1,34 @@
+//***************************************************************************
+//  FILENAME:	 AllenAssn6.cpp
+//  DESCRIPTION: Course Managemmt Program
+//
+//  DATE:        10/10/15
+//  CLASS/TERM:  CS362_X40_Data Structures - CU_CS362 - XIN_X40_15F8W1
+//  DESIGNER:	 Andrae Allen
+//  FUNCTIONS:	 main - reads values, performs calculations, & displays results
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx 
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx
+//				 xxxxxxxxxxxxxxxxxx
+//***************************************************************************
+
+#include <iostream> 
+#include <fstream>
+#include <cstdio> 
+#include <iomanip>
+#include <string>
+#include <cstddef>
+
+using namespace std;
+
+struct residence
+{
+	string phoneNumber;
+	float monthlyRent;
+	bool rented;
+};
+
