e89fd55746287ad39fb78cbd747173d969777666
2alpha <andrae3000@gmail.com>
2015-10-14 16:03:55 -0400

    add function

--- RU362WK7/Source.cpp
+++ RU362WK7/Source.cpp
@@ -40,19 +40,33 @@ struct residence
 
 
 const int IGNORE_AMOUNT = 100;
+const float  MAX_RENT = 999999;		// Max rental price - this was not specified
 
-string showAddDeleteExit();
+string showAddDelModExit();
 string convert2UpperCase(string stringInput);
-residence * ask2LoadExistingData(residence* residenceList);
+void ask2LoadExistingData(residence *& topOfList);
+
+void showRentals(string MainMenuChoice, residence *& topOfList, residence *& endOfList);
+void AddRentals2Array(string MainMenuChoice, residence *& topOfList, residence *& endOfList);
+string GetPhoneNumber();
 
 int main()
 {
 	residence * topOfList = NULL;
-	residence *newResidence = NULL;
-	residence *  residenceList = NULL;
+	residence * endOfList = NULL;
 
-	ask2LoadExistingData (residenceList);
-	
+	string MainMenuChoice;
+
+	ask2LoadExistingData(topOfList);
+
+	do
+	{
+		MainMenuChoice = showAddDelModExit();
+
+		showRentals(MainMenuChoice, topOfList, endOfList);
+		AddRentals2Array(MainMenuChoice, topOfList, endOfList);
+	}
+	while (MainMenuChoice != "X");
 
 	system("PAUSE");
 	return 0;
@@ -65,7 +79,7 @@ int main()
 //
 //  OUTPUT: 	  Return value: string response2Question - response to question
 //*************************************************************************
-string showAddDeleteExit()
+string showAddDelModExit()
 {
 	string response2Question;			// Response to question
 	bool repeatQuestion;				// Repeat question
@@ -73,9 +87,10 @@ string showAddDeleteExit()
 
 		cout << endl;
 		cout << "Please choose an option by entering its corresponding letter" << endl;
-		cout << "S = Show rentals to the Screen " << endl;
+		cout << "S = Show all rentals to the Screen " << endl;
 		cout << "A = Add rentals to the array " << endl;
 		cout << "D = Delete a rental" << endl;
+		cout << "M = Modify Monthly Rent" << endl;
 		cout << "X = Exit the program" << endl;
 		cout << endl;
 		cout << "Please enter response here: ";
@@ -85,7 +100,8 @@ string showAddDeleteExit()
 		response2Question = convert2UpperCase(response2Question);
 
 
-		if ((response2Question != "S") && (response2Question != "A") && (response2Question != "D") && (response2Question != "X"))
+		if ((response2Question != "S") && (response2Question != "A") && (response2Question != "D") && (response2Question != "M") 
+			&& (response2Question != "X"))
 		{
 			cout << endl;
 			cout << "ERROR! Unrecognized input, please try again." << endl;
@@ -102,8 +118,8 @@ string showAddDeleteExit()
 	// while repeat question equals true
 	while (repeatQuestion == true);
 
-
 	return response2Question;
+
 
 }
 
@@ -137,7 +153,7 @@ string convert2UpperCase(string stringInput)
 
 }
 
-residence * ask2LoadExistingData(residence* residenceList)
+void ask2LoadExistingData(residence *& topOfList)
 {
 	string promt1Response;
 	string fileName2Load;
@@ -150,7 +166,7 @@ residence * ask2LoadExistingData(residence* residenceList)
 	bool rented;
 
 	residence	* newNode = NULL,
-				* topOfList = NULL,
+				//* topOfList = NULL,
 				* endOfList = NULL;
 
 	ifstream inputFile1; // input file stream variable
@@ -256,20 +272,206 @@ residence * ask2LoadExistingData(residence* residenceList)
 
 	} while (numOfErrors > 0);
 
-	/////////////
-	residence *here = topOfList;
-	while (here != NULL) {                // while not at end of list… 
 
-                      // Increment count 
+}
 
-		cout << here->phoneNumber << endl;    // Print name in node pointed 
-									   // to by here pointer 
+void showRentals(string MainMenuChoice, residence *& topOfList, residence *& endOfList)
+{
+	if (MainMenuChoice == "S")
+	{ 
+		cout << fixed << showpoint << setprecision(2);
+		cout << "Phone Nmmber" << setw(15) << " Monthly Rent" << setw(15)  << "Status      " << endl;
+		cout << "------------" << setw(15) << "-------------" << setw(15)  << "------------" << endl;
 
-		here = here->next;             // Move here pointer to point 
-									   // to the next node in list 
-	} // end while 
+		residence *here = topOfList;
+		while (here != NULL) // while not at end of list… 
+		{                
+	
+			cout << here->phoneNumber << setw(15);    // Print phone number in node pointed 
+			cout << here->monthlyRent << setw(15);    // Print monthly in node pointed 
+
+			if (here->rented == 1)
+			{
+				cout << left;
+				cout << "   rented" << endl;
+				cout << right;
+			}
+
+			else
+			{
+				cout << left;
+				cout << "   available" << endl;
+				cout << right;
+			}
+
+
+			here = here->next;             // Move here pointer to point 
+			// to the next node in list 
+		} // end while 
+
+	}
+
+}
+
+void AddRentals2Array(string MainMenuChoice, residence *& topOfList, residence *& endOfList)
+{
+	if (MainMenuChoice == "A")
+	{
+		residence * newNode = NULL;
+		string validPhoneNumber;
+		
+		cout << "You selected Add Rental to array" << endl;
+		cout << "There are 3 steps to complete this entry, see below for instructions " << endl;
+		cout << "Step 1. Enter a vaild phone number in the format of \"###-###-####\" " << endl;
+		cout << "Step 2. Enter the monthly rent. Must greater than 0 and less than " << MAX_RENT << endl;
+		cout << "Step 3. Enter the Rental Status as Y = yes, N = No" << endl;
+		cout << endl;
+		
+		validPhoneNumber = GetPhoneNumber();
+
+		cout << "got NUmber" << endl; 
+
+		newNode = new (nothrow)residence;    // Allocate new node space
+
+		if (newNode == NULL)
+		{
+			cout << "Heap error - could not allocate memory" << endl;
+			system("pause");
+		}
+
+		else
+		{                        // Memory allocated put data into new node
+			newNode->phoneNumber = validPhoneNumber;
+			newNode->monthlyRent = 1111;
+			newNode->rented = 0;
+			newNode->next = NULL;
+
+			// Insert new node into the list
+			if (topOfList == NULL)			// If list is empty...
+			{
+				endOfList = newNode;          // point both pointers to new node 
+				topOfList = newNode;
+			}
+
+			else
+			{								// If list is not empty...
+				endOfList->next = newNode;    // Point Next ptr in last to new node
+				endOfList = newNode;          // and also point last to new node
+			}
+
+		}   // end else where newNode != NULL
 
 
 
-	return 0;
+
+
+
+
+	
+		cout << "Entry successfully added!" << endl << endl;
+		
+
+	}
+}
+string GetPhoneNumber()
+{
+	string phoneNumber;					// Phone Number 
+	int length;                         // Length of user input
+
+	int errorCounter;					// Error Counter
+
+	do
+	{
+		int i = 0;
+		errorCounter = 0;
+		cout << endl;
+		cout << "Step 1. Enter the phone number : ";
+		getline(cin, phoneNumber);
+
+		length = phoneNumber.length();
+
+		if (length < 12)
+		{
+			cout << "The phone number you entered is too short" << endl;
+			cout << "Please follow the format \"###-###-####\" Try again." << endl;
+			errorCounter++;
+		}
+
+		else if (length > 12)
+		{
+			cout << "The phone number you entered is too long" << endl;
+			cout << "Please follow the format \"###-###-####\" Try again." << endl;
+			errorCounter++;
+		}
+
+		else if (length = 12)
+		{
+
+			for (int index = 0; index < 3; index++)         // Used Loops for error checking 
+			{
+				if (!(isdigit(phoneNumber[index])))
+				{
+
+					errorCounter++;
+					//cout << "not a number " << endl; 
+				}
+
+			}
+
+
+			for (int index = 3; index < 4; index++)         // Used Loops for error checking 
+			{
+				if (phoneNumber[index] != '-')
+				{
+					errorCounter++;
+					//cout << "dash not where it should be " << endl;
+				}
+
+			}
+
+			for (int index = 4; index < 7; index++)         // Used Loops for error checking 
+			{
+				if (!(isdigit(phoneNumber[index])))
+				{
+					errorCounter++;
+					//cout << "not a number " << endl;
+				}
+
+			}
+
+			for (int index = 7; index < 8; index++)         // Used Loops for error checking 
+			{
+				if (phoneNumber[index] != '-')
+				{
+					errorCounter++;
+					//cout << "dash not where it should be " << endl;
+				}
+
+			}
+
+			for (int index = 8; index < 12; index++)         // Used Loops for error checking 
+			{
+				if (!(isdigit(phoneNumber[index])))
+				{
+					errorCounter++;
+					//cout << "not a number " << endl;
+				}
+
+			}
+
+		} /// end else if
+
+		if ((length == 12) && (errorCounter > 0))
+		{
+			cout << endl;
+			cout << "Error! You Enterd 12 characters, however not in the correct format." << endl;
+			cout << "Please follow the format \"###-###-####\" Try again." << endl;
+		}
+	}
+
+
+	while (errorCounter > 0); // loop if error counter is greater than zero (0) 
+
+	return phoneNumber;
+
 }
